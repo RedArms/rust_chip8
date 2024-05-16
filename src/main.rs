1aefd251ddb9469336bae552b6210ae7aa2359d9
RedArms <reda_1030@hotmail.com>
2024-05-16 14:58:10 +0200

    idk what i added since last push tbh

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-use std::{ops::{BitAnd, Not}, ptr::null};
-
+use std::{collections::VecDeque, ops::{BitAnd, Not}, ptr::null, sync::Arc};
+use rand::{self, Rng};
 
 fn main() {
     let RAM: [u16; 4095] = [0x00;0xFFF];
     let SCREEN : [[bool;31];63] = [[false;31];63];
-
+    
     println!("Hello, world!");
+    let mut cpu = CPU::init();
+    println!("i before: {}", cpu.I);
+    cpu.execute(0xAFFE);
+    println!("i after: {}", cpu.I);
+
+
+    println!("i before: {}", cpu.v2);
+    cpu.execute(0xC20F);
+    println!("i after: {}", cpu.v2);
 }
 
 
@@ -44,25 +53,34 @@ struct CPU{
     I:u16,
     PC:u16,
     SP:u8,
-    STACK:[u16; 16],
+    STACK:VecDeque<u16>,
     RAM:[u16; 4095],
     SCREEN:[[bool;31];63]
 }
 
 impl CPU {
 
+    fn init() -> CPU{
+        let cpu = CPU { v0: 0, v1: 0, v2: 0, v3: 0, v4: 0, v5: 0, v6: 0, v7: 0, v8: 0, v9: 0, vA: 0, vB: 0, vC: 0, vD: 0, vE: 0, vF: 0, I: 0, PC: 0, SP: 0, STACK: VecDeque::new(), RAM: [0;4095], SCREEN: [[false;31];63] };
+        return cpu;
+    }
+
+    fn fetch(&mut self) -> u16 {
+        return self.RAM[self.PC as usize];
+    }
+
     fn getXreg(&mut self,x:u8) -> &u8 {
         match x {
-            0=>return &self.v0,
-            1=>return &self.v1,
-            2=>return &self.v2,
-            3=>return &self.v3,
-            4=>return &self.v4,
-            5=>return &self.v5,
-            6=>return &self.v6,
-            7=>return &self.v7,
-            8=>return &self.v8,
-            9=>return &self.v9,
+            0x0=>return &self.v0,
+            0x1=>return &self.v1,
+            0x2=>return &self.v2,
+            0x3=>return &self.v3,
+            0x4=>return &self.v4,
+            0x5=>return &self.v5,
+            0x6=>return &self.v6,
+            0x7=>return &self.v7,
+            0x8=>return &self.v8,
+            0x9=>return &self.v9,
             0xA=>return &self.vA,
             0xB=>return &self.vB,
             0xC=>return &self.vC,
@@ -75,16 +93,16 @@ impl CPU {
 
     fn setXreg(&mut self,x:u8,value:u8) {
         match x {
-            0=>self.v0  = value,
-            1=>self.v1  = value,
-            2=>self.v2  = value,
-            3=>self.v3  = value,
-            4=>self.v4  = value,
-            5=>self.v5  = value,
-            6=>self.v6  = value,
-            7=>self.v7  = value,
-            8=>self.v8  = value,
-            9=>self.v9  = value,
+            0x0=>self.v0 = value,
+            0x1=>self.v1 = value,
+            0x2=>self.v2 = value,
+            0x3=>self.v3 = value,
+            0x4=>self.v4 = value,
+            0x5=>self.v5 = value,
+            0x6=>self.v6 = value,
+            0x7=>self.v7 = value,
+            0x8=>self.v8 = value,
+            0x9=>self.v9 = value,
             0xA=>self.vA = value,
             0xB=>self.vB = value,
             0xC=>self.vC = value,
@@ -109,7 +127,41 @@ impl CPU {
         let x234 = getXBytes(234, opcode);
 
         match instruction{
+             0=>{
+                if x3 == 0xE {
+                    if x4 == 0x0 {
+                        self.SCREEN = [[false;31];63];
+                    }
+                    else {
+                        self.PC = self.STACK.pop_front().unwrap();
+                        self.SP-=1;
+                    }
+                }
+                else {
+                    //SYS addr 0nnn
+                }
+             },
+             2=>{
+                 self.STACK.push_front(self.PC);
+                 self.PC = x234;
+             }
              1=>self.PC = opcode & 0b0000_1111_1111_1111,
+             
+             3=>{
+                if *self.getXreg(x2) == x34 {
+                    self.PC +=1
+                } 
+             },
+             4=>{
+                if *self.getXreg(x2) != x34 {
+                    self.PC +=1
+                } 
+             },
+             5=>{
+                if *self.getXreg(x2) == *self.getXreg(x3) {
+                    self.PC +=1
+                } 
+             },
              6=>{
                 self.setXreg(x2,x34);
              },
@@ -172,13 +224,36 @@ impl CPU {
                     }
 
                 },
+             9=>{
+                if *self.getXreg(x2) != *self.getXreg(x3) {
+                        self.PC +=1
+                } 
+             },
              0xA=>{
                 self.I = x234;
              },
-             
+             0xB=>{
+                self.PC = *self.getXreg(0) as u16 + x234;
+             },
+             0xC=>{
+                let mut rng = rand::thread_rng();
+                self.setXreg(x2, rng.gen_range(0x00..0xFF) & x34);
+             }
+
                 _=>print!("y'a rien")
             }
         }
             
             
     }
+
+//   struct Stack<T>{
+//       stack:Vec<T>,
+//       last:usize
+//   }
+//
+//   impl<T> Stack<T> {
+//       fn init() -> Stack<T>{
+//           
+//       }
+//   }
